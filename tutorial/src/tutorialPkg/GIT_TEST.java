7399d5c4c33e096f0a320c457160bf825da2a4e6
victor.truong <victor.truong@091DA-1182.admin.cssd.ab.ca>
2018-11-12 13:05:40 -0700

    testint new push

--- tutorial/src/tutorialPkg/GIT_TEST.java
+++ tutorial/src/tutorialPkg/GIT_TEST.java
@@ -4,7 +4,7 @@ public class GIT_TEST {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("IT WORKS");
+		System.out.println("IT doesnt WORKS");
 
 	}
 
